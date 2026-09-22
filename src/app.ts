dcd7a9c52b83d3fe3a8f44990f467fd439b1bc70
ligyanicolly <ligyageorges@gmail.com>
2026-09-22 15:32:32 -0300

    Projeto-Backend

--- src/app.ts
+++ src/app.ts
@@ -2,67 +2,146 @@
 // O Express será utilizado para criar o servidor web
 import express from "express";
 import type { Express, Request, Response } from "express";
-// importa a classe Player do arquivo Player.ts
+
+import fs from "fs"; 
+// Importa o módulo fs para manipulação de arquivos
+// importa a classe Player pro arquivo.ts
 import { Player } from "./models/Player.js";
 
 // Cria uma aplicação Express
 // A função express() devolve um objeto que representa o servidor da aplicação
-const app: Express = express(); 
+const app: Express = express();
 
-// middleware para permitir que o servidor aceite requisições com corpo em formato JSON
+// Middlware para garantir que o servidor entenda requisições com corpo em JSON
 app.use(express.json());
+
 // Define a porta onde o servidor ficará disponível
 // Neste caso, o servidor poderá ser acessado pela porta 8081
 const PORT: number = 8081;
 
-// instanciação de um jogador utilizando a classe "Player"
-// criamos (instanciamos) um novo jogador chamado "Hero" com 100 de saúde e nível 1
-// a partir da classe Player, que foi importada do arquivo Player.ts
-let player1: Player = new Player("Hero", 100, 5);
+//Define o nome do arquivo do diretório onde os arquivos serão armazenados
+const DATA_FILE = "./data/players.json";
 
-// rota GET para obter informações do jogador
-// quando o usuário acessar a rota "/player", o servidor responderá com os dados do jogador
-// a função de callback recebe dois parâmetros: req (requisição) e res (resposta)
+/*
+Função para garantir que o diretório de dados exista antes de tentar salvar arquivos.
+Se o diretório não existir, ele será criado.
+*/
+function ensureDataDirectoryExists() {
+    const dataDir = "./data";
+    if (!fs.existsSync(dataDir)) {
+        fs.mkdirSync(dataDir);
+    }
+}
 
+//Chamada da função para garantir que o diretório de dados exista
+//Antes de quaquer operação de leitura ou escrita no arquivo
+ensureDataDirectoryExists();
+
+//Função para salvar os dados do jogador em um arquivo JSON
+function savePlayerState(player: Player) {
+    //Converte o objeto player em uma string JSON
+    const playerData = JSON.stringify(player, null, 2);
+    //Salva a string JSON no arquivo definido em DATA_FILE
+    fs.writeFileSync(DATA_FILE, playerData, "utf-8"); //SALVAR, PASSAR INFORMAÇÃO
+}
+
+//Função para carregar os dados do jogador a partir de um arquivo JSON
+function loadPlayerState(): Player {
+    //Verifica se o arquivo de dados existe
+    if (fs.existsSync(DATA_FILE)) {
+        //Lê o conteúdo do arquivo e converte de volta para um objeto Player
+        const data = fs.readFileSync(DATA_FILE, "utf-8");
+        const playerData = JSON.parse(data);
+
+        /*ATENÇÃO: JSON.parse() retorna um objto "puro" (sem os metodos da classe Player).
+        Para que o objeto tenha os métodos da classe Player, precisamos criar uma nova instância da classe Player e 
+        passar os dados carregados para o construtor.*/
+        return new Player(playerData.name, playerData.health, playerData.level);
+    }
+    //Cria um novo player com valores padrão caso o arquivo não exista
+    const newPlayer: Player = new Player("Hero", 100, 1);
+    savePlayerState(newPlayer);
+    return newPlayer;
+}
+//Inicializa o player carregando seu estado do arquivo JSON
+let player1: Player = loadPlayerState();
+
+//Rota GET para obter informações do jogador
+//Quando o usuário acessar a rota "/player", o servidor respondera com os dados do jogador
+//A função de callback recebe dois parâmetros: req (requisição) e res (resposta)
 app.get("/player", (req: Request, res: Response) => {
     res.json({
-        message: "Informações do jogador",
-        player: player1
+        massage: "Informação do jogador",
+        player:player1
+    });
+})
+
+//Rota POST para jogador atacar quando o usuário acessar a rota "/player/attack", o servidor chamará o método attack() do jogador
+//é utilizada para enviar dados ou realizar ações que alteram o estado do servidor, como neste caso
+//onde o jogador realiza uma ação (como acionar um comportamento de ataque), que é o método attack() do jogador.
+// //a função de callback recebe dois parâmetros: req (requisição) e res (resposta)
+app.post("/player/attack", (req: Request, res:Response) =>{
+    const attackMessage = player1.attack(); //Chama o método de attack
+    //Retorma uma resposta JSON com a maensagem do ataque 
+    res.json({
+        message: attackMessage,
     });
 });
 
-app.post("/player/attack", (req: Request, res: Response) => {
-    const attackMessage = player1.attack(); 
-        res.json({
-        message: attackMessage,
-    });
-});   
-
-//rota POST para o jogador atacar
-// quando o usuário acessar a rota "/player/attack", o servidor chamará o método attack() do jogador
-// é utilizada para enviar dados ou realizar ações que alteram o estado do servidor, como neste caso
+//Rota POST para jogador receber dano
+//Quando o usuário acessar a rota "/player/attack", o servidor chamará o método attack() do jogador
+//é utilizada para enviar dados ou realizar ações que alteram o estado do servidor, como neste caso
 //onde o jogador realiza uma ação (como acionar um comportamento de ataque), que é o método attack() do jogador.
-// a função de callback recebe dois parâmetros: req (requisição) e res (resposta)
-
-app.post("/player/takeDamage", (req: Request, res: Response) => {
+//a função de callback recebe dois parâmetros: req (requisição) e res (resposta)
+app.post("/player/take-damage", (req: Request, res:Response) => {
+    //Extrai o valor do dano da requisição
     const { damage } = req.body;
-    const damageMessage = player1.takeDamage(damage);
+    //Chama o método takeDamege() do jogador
+    const damageMassage = player1.takeDamage(damage);
+    //Salva o estado atual do jogador no arquivo JSON
+    savePlayerState(player1);
+    //Retorma uma resposta JSON com a maensagem do dano e o estado atual do jogador para o cliente que fez a requisição
     res.json({
-        action: damageMessage,
+        //Retorna a mensagem do dano recebido 
+        action: damageMassage,
+        //Retorna a saúde atual do jogador
+        currentHealth: player1.health,
+        //Retorna o nível atual do jogador 
+        currentLevel: player1.level
+    });
+});
+
+app.post("/player/take-health", (req: Request, res:Response) => {   
+    const { health } = req.body;
+    const healthMessage = player1.takeHealth(health);
+    savePlayerState(player1);
+    res.json({
+        action: healthMessage,
         currentHealth: player1.health,
         currentLevel: player1.level
-    })
-})
+    });
+});
+
+app.post("/player/up-level", (req: Request, res:Response) => {
+    const { level } = req.body;
+    const levelMessage = player1.upLevel(level);
+    savePlayerState(player1);
+    res.json({
+        action: levelMessage,
+        currentHealth: player1.health,
+        currentLevel: player1.level
+    });
+});
 
 // Inicializa o servidor utilizando a porta definida
 // O método listen() faz o servidor começar a "escutar" requisições HTTP
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
-
-console.log("Rotas disponíveis:");
-console.log(`Get http://localhost:${PORT}/player/attack - Obter informações do jogador`);
-console.log (`POST http://localhost:${PORT}/player/attack - Jogador realiza um ataque`);
-console.log(`POST http://localhost:${PORT}/player/take-damage - jogador recebe um dano`);
+    console.log("Rotas disponiveis:");
+    console.log(`GET http://localhost:${PORT}/player - Obter informações do jogador`);
+    console.log(`POST http://localhost:${PORT}/player/attack - Jogador realiza um ataque`);
+    console.log(`POST http://localhost:${PORT}/player/take-damage - Jogador recebe dano`);
+    console.log(`POST http://localhost:${PORT}/player/take-health - Jogador recebe cura`);
+    console.log(`POST http://localhost:${PORT}/player/up-level - Jogador sobe de nível`);
 });
-
--- src/models/Player.ts
+++ src/models/Player.ts
@@ -1,32 +1,51 @@
-// A palavra "class" define que estamos criando um molde.
-// A palavra "export" permite que esse arquivo seja usado por outros arquivos (como o app.ts)
+// A palavra "class" define que estamos criando um molde 
+// A palavra "export" permite que esse arquivo seja usado por ooutros arquivos (como app.ts)
 export class Player {
-    public name: string; // O nome do jogador em texto
-    public health: number; // A saúde do jogador em número
-    public level: number; // O nível do jogador em número
+    public name: string; //o nome do jogador (texto)
+    public health: number; //a saúde do jogador (numero)
+    public level: number; //o nivel do jogador (número)
 
-    // Construtores (o construtor é um método especial que é executado automaticamente quando a classe é instanciada uma única vez)
-    constructor(name: string, health: number = 100, level: number = 1) {
-        this.name = name; // A palavra "this" faz referência a própria classe, ou seja: "pegue o atributo 'name' da classe Player e atribua o valor do parâmetro 'name' a ele"
+    // Consultores (O consultor é um método especial que executados automaticamnete quando a classe é instaciada uma única vez)
+    constructor(name: string, health: number = 100, level = 1) {
+        // A palavra "this" faz referencia a própria classe, ou seja: "Pegue o atibuto 'name' da classe Player e atribua o valor do parametro 'name' a ele"
+        this.name = name;
         this.health = health;
-        this.level = level
+        this.level = level;
     }
 
-    // Métodos (comportamentos de classe)
-    // Métodos são as "funções" que a classe pode executar, ou seja, são os comportamentos da classe.
-    // O método "attack" é um método que retorna um string.
+    //Metodos (verbos) de comportamentos de classe
+    //Métodos são "funções" que a classe pode executar, ou seja, são os complortamentos de classe.
+    //O método "attack" é um método que retorna a uma string.
     public attack(): string {
-        const damage = this.level * 10; // Calcula o dano baseado no nível do jogador 
-        return `${this.name} atacou e causou ${damage} de dano`;
+        const damage = this.level * 10; //Calcule o dano baseado no nível do jogador
+        return `${this.name} atacou e causou ${damage} de dano!`;
     }
-    // O método "takeDamage" é um método que recebe um número como parâmetro e não retorna nada (void).
-    public takeDamage(amount: number): string {
-        this.health -= amount; // Reduz a saúde do jogador pelo valor do parâmetro
-        if (this.health < 0) {
-            this.health = 0; // Garante que a saúde não fique negativa
-            return `${this.name} foi derrotado!`;
-        }
 
-        return `${this.name} recebeu ${amount} de dano e agora tem ${this.health} de saúde.`;
+    //O método "takeDamage" é um método que recebe um número como parametro e não retorna na (void).
+    public takeDamage(damage: number): string {
+        this.health -= damage; //Reduz a saúde do jogador pelo valor do paranetro
+        //Regra para garantir que a saúde não fique negativa 
+        if (this.health < 0) {
+            this.health = 0; //Garante que a saúde não fique negativa
+            return `${this.name} foi derrotado`;
+        }
+        
+        return `${this.name} recebeu ${damage} de dano e agora tem ${this.health} de saúde`;
     }
-}''
+
+    public takeHealth( health: number): string {
+        this.health += health; //Aumenta a saúde do jogador pelo valor do paranetro
+        if (this.health > 100) {
+            this.health = 100; //Garante que a saúde não ultrapasse 100
+        }
+        return `${this.name} recebeu ${health} de cura e agora tem ${this.health} de vida`;
+    }
+
+    public upLevel(level: number): string {
+        this.level += level; //Aumenta o nível do jogador pelo valor do parâmetro.
+        if (this.level >= 100) {
+            this.level = 100; //Garante que a saúde não ultrapasse 100
+        }
+        return `${this.name} subiu para o nível ${this.level}`;
+    }
+}
